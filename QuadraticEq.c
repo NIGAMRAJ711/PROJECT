cef757ec0d5c503e066cb77d183df9c2f276edb2
Shivaraj9600 <1nt23is201.shivaraj@nmit.ac.in>
2025-01-01 19:01:04 +0530

    shivaraj changed code

--- QuadraticEq.c
+++ QuadraticEq.c
@@ -16,8 +16,7 @@ int main() {
         printf("Root 1 = %.2f and root 2 = %.2f\n", root1, root2);
     } else if (discriminant == 0) {
         root1 = root2 = -b / (2 * a);
-        printf("Roots are real and equal.\n");
-        printf("Root 1 = Root 2 = %.2f\n", root1);
+        printf("Roots are real and equal = %.2f\n", root1);
     } else {
         printf("Roots are imaginary.\n");
     }
